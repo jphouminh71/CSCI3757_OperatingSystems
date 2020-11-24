73bac11468e989cde8b5e937f923b57694346835
JonathanPhouminh <jonathanphouminh@Jonathans-MacBook-Pro.local>
2020-11-24 15:31:10 -0700

    pa4

---
+++ PA4/PA4/pager-lru.c
@@ -0,0 +1,94 @@
+/*
+ * File: pager-lru.c
+ * Author:       Andy Sayler
+ *               http://www.andysayler.com
+ * Adopted From: Dr. Alva Couch
+ *               http://www.cs.tufts.edu/~couch/
+ *
+ * Project: CSCI 3753 Programming Assignment 4
+ * Create Date: Unknown
+ * Modify Date: 2012/04/03
+ * Description:
+ * 	This file contains an lru pageit
+ *      implmentation.
+ */
+
+#include <stdio.h> 
+#include <stdlib.h>
+
+#include "simulator.h"
+
+void pageit(Pentry q[MAXPROCESSES]) { 
+    /* This file contains the stub for an LRU pager */
+    /* You may need to add/remove/modify any part of this file */
+
+    /* Static vars */
+    static int initialized = 0;
+    static int tick = 1; // artificial time
+    static int timestamps[MAXPROCESSES][MAXPROCPAGES];
+
+    /* Local vars */
+    int proctmp;
+    int pagetmp;
+
+    /* initialize static vars on first run */
+    if(!initialized){
+	for(proctmp=0; proctmp < MAXPROCESSES; proctmp++){
+	    for(pagetmp=0; pagetmp < MAXPROCPAGES; pagetmp++){
+		timestamps[proctmp][pagetmp] = 0; 
+	    }
+	}
+	initialized = 1;
+    }
+
+    /* Local vars */
+    int proc;
+    int pc;
+    int page;
+    int oldpage; 
+    
+    for(proc=0; proc<MAXPROCESSES; proc++) { 
+        /* Is process active? */
+        if(q[proc].active) {
+            /* Dedicate all work to first active process*/ 
+            pc = q[proc].pc; 		        // program counter for process
+            page = pc/PAGESIZE; 		// page the program counter needs
+            timestamps[proc][page] = tick;            // update timestamp for the current page 
+
+            /* Is page swaped-out? */
+            if(!q[proc].pages[page]) {
+                /* Try to swap in */
+                printf("HELLO");
+                if(!pagein(proc,page)) {
+                     
+                    /* LRU algorithm, look back at the processes and look for the one with the most ticks 
+                        * Follows the idea of local replacement
+                        * Looks through current processes available pages and checks if they are swapped in
+                        * Looks for that set of pages timestamps and look for the least recently used, 
+                           should be the one with the timestamp greater than the current page we are trying to swap
+                    */
+
+                   printf("HELLO");
+                   int maxTime = timestamps[proc][page];
+                   int pageToSwap = MAXPROCPAGES; // start at the end and going to loop through it 
+                   
+
+                    // look through for a page that has an older time 
+                    for(oldpage=pageToSwap; oldpage > 0; oldpage--) {
+                        if(timestamps[proc][oldpage] > maxTime) {
+                            maxTime = timestamps[proc][oldpage];
+                            pageToSwap = oldpage;
+                        }
+                    }
+                    pageout(proc, pageToSwap);
+                }
+            }
+            /* Break loop after finding first active process */
+            break;
+        }
+    } 
+    
+
+    /* advance time for next pageit iteration */
+    tick++;
+} 
